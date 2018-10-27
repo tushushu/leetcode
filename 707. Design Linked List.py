cad17007bc122e1863a352681cbaa7a018f6bbbd
tushushu <m18591915960@163.com>
2018-10-27 09:31:11 +0800

    daily practice

---
+++ 707. Design Linked List.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2018-10-27 09:18:26
+@Last Modified by:   tushushu
+@Last Modified time: 2018-10-27 09:18:26
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        self.length = 0
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index >= self.length:
+            return -1
+        node = self.head
+        for _ in range(index):
+            node = node.next
+        return node.val
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        node = self.head
+        for _ in range(self.length - 1):
+            node = node.next
+        node.next = Node(val)
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
