c563021fee63222ced392930303dedcc861aedcc
tushushu <m18591915960@163.com>
2018-11-14 10:33:54 +0800

    daily practice

---
+++ 344. Reverse String.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2018-11-14 10:32:34
+@Last Modified by:   tushushu
+@Last Modified time: 2018-11-14 10:32:34
+"""
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        return s[::-1]
