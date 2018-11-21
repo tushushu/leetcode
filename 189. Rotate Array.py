d3c1c728401555c81c837f876f665b521de76045
tushushu <m18591915960@163.com>
2018-11-21 11:16:17 +0800

    daily practice

---
+++ 189. Rotate Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        m = n - k
+        for _ in range(m):
+            nums.append(nums.pop(0))
