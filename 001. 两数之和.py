f2adbbc930f1eeb011477d3e00a3cea67c705d93
tushushu <m18591915960@163.com>
2018-12-19 16:14:56 +0800

    daily practice

---
+++ 001. 两数之和.py
@@ -0,0 +1,19 @@
+"""
+@Author: tushushu
+@Date: 2018-12-19
+"""
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target - num] = i
