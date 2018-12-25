e608513d017f0c661820ffadbbb00b054b303f90
tushushu <m18591915960@163.com>
2018-12-25 11:08:23 +0800

    daily practice

---
+++ 003. 无重复字符的最长子串.py
@@ -0,0 +1,29 @@
+"""
+@Author: tushushu
+@Date: 2018-12-25 10:28:49
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == "":
+            return 0
+        dic = {}
+        ret = 0
+        start = 0
+        for end, c in enumerate(s):
+            if c in dic and dic[c] >= start:
+                ret = max(ret, end - start)
+                start = dic[c] + 1
+            dic[c] = end
+        return max(ret, end - start + 1)
+
+
+if __name__ == "__main__":
+    t = Solution()
+    s = " "
+    print(t.lengthOfLongestSubstring(s))
