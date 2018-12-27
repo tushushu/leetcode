4ff38ca86daf3d14cbee468cdace91fd9c8b1220
tushushu <m18591915960@163.com>
2018-12-27 11:07:41 +0800

    daily practice

--- 004. Median of Two Sorted Arrays.py
+++ 004. Median of Two Sorted Arrays.py
@@ -1,57 +1,81 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jan  5 14:29:54 2018
-
-@author: Administrator
+@Author: tushushu
+@Date: 2018-12-26 10:17:06
 """
 
 
 class Solution:
-    def findMedianSortedArrays1(self, nums1, nums2):
-        a = len(nums1)
-        b = len(nums2)
-        k = 0
-        n, m = divmod(a + b, 2)
-        nums1.append(float('inf'))
-        nums2.append(float('inf'))
-        for i in range(n):
-            if nums1[0] < nums2[0]:
-                k = nums1.pop(0)
-            else:
-                k = nums2.pop(0)
-
-        res = min(nums1[0], nums2[0])
-        if m is 0:
-            res = (k + res) / 2
-        return res
-
-
     def findMedianSortedArrays(self, nums1, nums2):
-        nums1.append(float('inf'))
-        nums2.append(float('inf'))
-        nums1.insert(0, -float('inf'))
-        nums2.insert(0, -float('inf'))
-
-        l1, l2 = len(nums1), len(nums2)
-        n, odd = divmod(l1 + l2, 2)
-        low = 0
-        high = l1 - 1
-
-        while(low <= high):
-            i1 = (low + high) // 2
-            i2 = n - i1 - 2
-            if i2 < -1:
-                high = i1 - 1
-            elif i2 > l2 - 1:
-                low = i1 + 1                
-            elif i2 is not l2 - 1 and nums1[i1] > nums2[i2+1]:
-                high = i1 - 1             
-            elif i2 is not -1 and nums2[i2] > nums1[i1+1]:
-                low = i1 + 1
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m = len(nums1)
+        n = len(nums2)
+        if m < n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+        high = (m + n) // 2 - 1
+        tot = high - 1
+        low = tot - n + 1
+        while 1:
+            i = (low + high) // 2
+            j = tot - i
+            cond1 = (i == -1 or j == n - 1 or nums1[i] <= nums2[j + 1])
+            cond2 = (j == -1 or i == m - 1 or nums2[j] <= nums1[i + 1])
+            if cond1 and cond2:
+                if (m + n) % 2:
+                    if j == n - 1:
+                        ret = nums1[i + 1]
+                    elif i == m - 1:
+                        ret = nums2[j + 1]
+                    else:
+                        ret = min(nums1[i + 1], nums2[j + 1])
+                else:
+                    if j == -1:
+                        left = nums1[i]
+                    elif i == -1:
+                        left = nums2[j]
+                    else:
+                        left = max(nums1[i], nums2[j])
+                    if j == n - 1:
+                        right = nums1[i + 1]
+                    elif i == m - 1:
+                        right = nums2[j + 1]
+                    else:
+                        right = min(nums1[i + 1], nums2[j + 1])
+                    ret = (left + right) / 2
+                return ret
+            elif cond1:
+                low = i + 1
             else:
-                break
-        if odd:
-            res = min(nums1[i1+1], nums2[i2+1])
-        else:
-            res = (max(nums1[i1], nums2[i2]) + min(nums1[i1+1], nums2[i2+1])) / 2
-        return res
+                high = i - 1
+
+
+if __name__ == "__main__":
+    t = Solution()
+    nums1 = [1, 2, 3, 6]
+    nums2 = [4, 5, 8, 9]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = [1, 2, 3, 6]
+    nums2 = [4, 5, 8]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = [1]
+    nums2 = [2]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = [1, 3]
+    nums2 = [2]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = [1, 3]
+    nums2 = [2]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = []
+    nums2 = [2]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = []
+    nums2 = [2, 4]
+    print(t.findMedianSortedArrays(nums1, nums2))
+    nums1 = [1]
+    nums2 = [1]
+    print(t.findMedianSortedArrays(nums1, nums2))
