369bfcda29603e90153eb8e1b7951b0372db16b2
tushushu <m18591915960@163.com>
2019-02-11 15:22:14 +0800

    daily practice

---
+++ 027. 移除元素.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2019-02-11 15:12:02
+"""
+
+
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        for num in nums:
+            if num == val:
+                pass
+            else:
+                nums[i] = num
+                i += 1
+        return i
