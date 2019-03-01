a4e2e91576bc5166ff6f10065546d3a007ef3763
tushushu <m18591915960@163.com>
2019-03-01 18:03:08 +0800

    daily practice

--- 005. 最长回文子串.py
+++ 005. 最长回文子串.py
@@ -13,27 +13,46 @@ class Solution:
         """
 
         length = -float("inf")
-        ret = ""
+        mid = 0
+        width = 0
+        is_odd = True
         n = len(s)
         for i in range(n):
             # 奇数
-            j = 1
-            while i - j >= 0 and i + j < n:
-                if s[i - j] != s[i + j]:
-                    break
-                j += 1
-            if (j - 1) * 2 + 1 >= length:
-                length = (j - 1) * 2 + 1
-                ret = s[i - j + 1: i + j]
+            # 提前终止
+            max_len = min(n - 1 - i, i) * 2 + 1
+            if max_len > length:
+                j = 1
+                while i - j >= 0 and i + j < n:
+                    if s[i - j] != s[i + j]:
+                        break
+                    j += 1
+                new_len = (j - 1) * 2 + 1
+                if new_len >= length:
+                    length = new_len
+                    mid = i
+                    width = j
+                    is_odd = True
+
             # 偶数
+            # 提前终止
             j = 0
-            while i - j >= 0 and i + j + 1 < n:
-                if s[i - j] != s[i + j + 1]:
-                    break
-                j += 1
-            if j * 2 >= length:
-                length = j * 2
-                ret = s[i - j + 1: i + j + 1]
+            max_len = min(n - i, i + 1) * 2
+            if max_len > length:
+                while i - j >= 0 and i + j + 1 < n:
+                    if s[i - j] != s[i + j + 1]:
+                        break
+                    j += 1
+                new_len = j * 2
+                if new_len >= length:
+                    length = new_len
+                    mid = i
+                    width = j
+                    is_odd = False
+        if is_odd:
+            ret = s[mid - width + 1: mid + width]
+        else:
+            ret = s[mid - width + 1: mid + width + 1]
         return ret
 
 
--- 007. 整数反转.py
+++ 007. 整数反转.py
@@ -3,39 +3,23 @@
 @Author: tushushu
 @Date: 2018-12-29 10:20:08
 """
-MAX = 2**31 - 1
-MIN = -2**31
+from functools import reduce
+MAX = 2**31
+MIN = -2**31 - 1
 
 
 class Solution:
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-
-        digits = []
-        if x < 0:
-            num = -x
-            is_negative = True
-        else:
-            num = x
-            is_negative = False
-        while num != 0:
+    def helper(self, num):
+        if num is 0:
+            yield 0
+        while num is not 0:
             num, mod = divmod(num, 10)
-            digits.append(mod)
-        n = len(digits)
-        times = 1
-        ret = 0
-        for i in range(n - 1, -1, -1):
-            ret += times * digits[i]
-            times *= 10
-        if ret > MAX or ret < MIN:
-            ret = 0
-        else:
-            if is_negative:
-                ret = -ret
-        return ret
+            yield mod
+
+    def reverse(self, x):
+        ret = reduce(lambda x, y: x * 10 + y,
+                     self.helper(abs(x))) * [1, -1][x < 0]
+        return [0, ret][MIN < ret < MAX]
 
 
 if __name__ == "__main__":
