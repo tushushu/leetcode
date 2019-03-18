b8cd383432888c51b806c06bb5c667809af5b3f5
tushushu <m18591915960@163.com>
2019-03-18 13:30:46 +0800

    daily practice

--- 027. 移除元素.py
+++ 027. 移除元素.py
@@ -7,14 +7,9 @@
 
 class Solution:
     def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
         i = 0
         for num in nums:
-            if num == val:
+            if num is val:
                 pass
             else:
                 nums[i] = num
