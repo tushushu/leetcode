a19cee3ac032cb91e5557b9e5a12ff716d98aebb
tushushu <m18591915960@163.com>
2019-08-18 13:45:18 +0800

    一遍过

---
+++ python/116. 填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2019-08-18 13:28:18
+"""
+
+# Definition for a Node.
+
+
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Node) -> Node:
+        if root is None:
+            return
+        if root.left:
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
