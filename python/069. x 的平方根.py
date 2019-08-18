7dfdc4da3d3819b5677a195790d7e1afe2ce9bc8
tushushu <m18591915960@163.com>
2019-08-18 14:30:10 +0800

    精简逻辑

--- python/069. x 的平方根.py
+++ python/069. x 的平方根.py
@@ -7,9 +7,7 @@
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:
-            return x
-        low = 1
+        low = 0
         high = x
         while low <= high:
             mid = (low + high) // 2
