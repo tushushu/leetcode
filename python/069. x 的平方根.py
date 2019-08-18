52aa7f1ebd78ae3a0a9e17f3ee2465fad6c84d17
tushushu <m18591915960@163.com>
2019-08-18 14:28:42 +0800

    完成

---
+++ python/069. x 的平方根.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2019-08-18 14:12:07
+"""
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        low = 1
+        high = x
+        while low <= high:
+            mid = (low + high) // 2
+            if x >= mid * mid and x < (mid + 1) * (mid + 1):
+                return mid
+            elif x < mid * mid:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+
+if __name__ == "__main__":
+    t = Solution()
+    print(t.mySqrt(9))
