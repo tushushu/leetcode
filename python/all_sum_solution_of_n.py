b4846c5943eae77203d56977cc8fb18e392ea108
tushushu <m18591915960@163.com>
2019-10-10 17:54:07 +0800

    用Cython提升性能

--- python/all_sum_solution_of_n.py
+++ python/all_sum_solution_of_n.py
@@ -30,3 +30,38 @@ class FastSolution:
         self.ret = []
         self.helper(m, [], 0, 1, min(m, n))
         return self.ret
+
+
+"""
+# 在Jupyter Notebook中运行
+%%cython --cplus
+from libcpp.vector cimport vector
+
+cdef inline fmin(int x, int y):
+    if x < y:
+        return x
+    return y
+
+cdef class FasterSolution:
+    cdef void helper(self, int target, vector[int] solution, int total, int start, int end, vector[vector[int]] &ret):
+        if total == target:
+            ret.push_back(solution)
+            return
+        if total > target or start > end:
+            return
+        # 不使用当前数字，提升搜索下限
+        self.helper(target, solution, total, start+1, end, ret)
+        # 使用当前数字，提升下限同时降低上限
+        total += start
+        end = target - total  # 这一步优化是关键
+        solution.push_back(start)
+        self.helper(target, solution, total, start+1, end, ret)
+
+    cdef vector[vector[int]] _func(self, int m, int n):
+        cdef vector[vector[int]] ret
+        self.helper(m, [], 0, 1, fmin(m, n), ret)
+        return ret
+    
+    def func(self, int m, int n):
+        return self._func(m, n)
+"""
