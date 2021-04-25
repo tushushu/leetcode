165e1b88f65584d0bf80fc9b4000ba90ec70627a
tushushu <m18591915960@163.com>
2021-04-25 20:47:03 +0800

    daily practice

--- python/064. 最小路径和.py
+++ python/064. 最小路径和.py
@@ -3,21 +3,25 @@
 @Author: tushushu
 @Date: 2019-04-10 15:44:40
 """
+from typing import List
 
 
 class Solution:
-    def minPathSum(self, grid):
-        if not grid:
-            return 0
+    def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        ret = [[0 for _ in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                
-
-if __name__ == "__main__":
-    t = Solution()
-    # grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
-    grid = []
-    t.minPathSum(grid)
+                if i == 0:
+                    if j == 0:
+                        dp[i][j] = grid[i][j]
+                    else:
+                        dp[i][j] = dp[i][j - 1] + grid[i][j]
+                else:
+                    if j == 0:
+                        dp[i][j] = dp[i - 1][j] + grid[i][j]
+                    else:
+                        dp[i][j] = min(dp[i][j - 1] + grid[i][j],
+                                       dp[i - 1][j] + grid[i][j])
+        return dp[i][j]
